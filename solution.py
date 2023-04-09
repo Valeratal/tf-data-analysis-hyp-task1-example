394241e0514f51a594157e4ca1d80744b0f5d1ad
Valeratal <test4you@yandex.ru>
2023-04-09 21:53:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,13 +5,15 @@ import numpy as np
 chat_id = 252926140 # Ваш chat ID, не меняйте название переменной
 
 
-import scipy.stats as st
+import scipy.stats as stats
 
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-  p_control = x_success / x_cnt
-  p_test = y_success / y_cnt
-  p_pool = (x_success + y_success) / (x_cnt + y_cnt)
-  se = np.sqrt(p_pool * (1 - p_pool) * (1/x_cnt + 1/y_cnt))
-  z_score = (p_test - p_control) / se
-  p_value = st.norm.sf(abs(z_score))
-  return p_value < 0.06
+  p1 = x_success / x_cnt
+  p2 = y_success / y_cnt
+  p = (x_success + y_success) / (x_cnt + y_cnt)
+  se = np.sqrt(p * (1 - p) * (1/x_cnt + 1/y_cnt))
+  z = (p2 - p1) / se
+  if z > stats.norm.ppf(1 - 0.06/2):
+    return True
+  else:
+    return False
