3e4a27d37101498ac71212d31d25b79f0162cb46
Valeratal <test4you@yandex.ru>
2023-04-09 20:56:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,11 +4,15 @@ import numpy as np
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+import scipy.stats as stats
+
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    # Calculate the expected number of successful calls in the test sample under the null hypothesis
+    p = x_cnt / y_cnt
+    expected_success = y_cnt * p
+
+    # Calculate the p-value for the hypothesis test
+    p_value = 1 - stats.binom.cdf(x_success - 1, y_cnt, p)
+
+    # Compare the p-value to the significance level and return the decision
+    return p_value < 0.06 and x_success > expected_success
